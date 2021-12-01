c8d08a9f5782cce8d08d2e5a1f58a2560cef8e1d
Aaron Luna <contact@aaronluna.dev>
2021-11-30 16:20:53 -0800

    add routes folder for dev/test troubleshooting

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style lang="postcss">
+	:global(body) {
+		font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial,
+			Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+		background-color: hsl(226, 27%, 10%);
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import SimpleTable from '$lib/components/SimpleTable.svelte';
+	import type { TableSettings } from '$lib/types';
+	import { pfxBarrelColumnSettings } from '$lib/__tests__/data/columnSettings';
+	import { barrelsForDateMockData } from '$lib/__tests__/data/getBarrelsForDate';
+
+	const settings: TableSettings = {
+		showHeader: true,
+		header: 'Barrels',
+		showSortDescription: true,
+		sortBy: 'time_pitch_thrown_est',
+		sortDir: 'desc',
+		paginated: true,
+		pagination: {
+			pageSize: 5,
+			pageSizeOptions: [5, 10, 15, 20, 25],
+			pageNavLayout: 'compact',
+			rowTypeSingle: 'barrel',
+			rowTypePlural: 'barrels',
+		},
+	};
+</script>
+
+<SimpleTable data={barrelsForDateMockData} columns={pfxBarrelColumnSettings} {settings} />
