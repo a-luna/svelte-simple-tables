d18d25c232dcd4432f8223ff60da45789124be5c
Aaron Luna <contact@aaronluna.dev>
2022-01-05 11:46:53 +0000

    build: :package: add syncWidth store to exports

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,3 +1,4 @@
 export { createComponentWidthStore } from './componentWidth';
 export { pageWidth } from './pageWidth';
+export { syncWidth } from './syncWidth';
 export { createTableStateStore } from './tableState';
